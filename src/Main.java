03223510db051b75250fcf39a93b73956d48f486
greysparks00 <greysparks00@gmail.com>
2026-09-25 08:37:06 -0500

    refactor into smaller source file

--- src/Main.java
+++ src/Main.java
@@ -2,35 +2,32 @@ import rendering.Renderer;
 import window.FramebufferPresenter;
 import window.WindowService;
 
-public class Main {
+void main() {
+    // create
+    WindowService window = new WindowService(1024, 768);
+    window.init();
 
-    public static void main(String[] args) {
-        // create
-        WindowService window = new WindowService(1024, 768);
-        window.init();
+    Renderer renderer = new Renderer(
+            window.getWidth(),
+            window.getHeight()
+    );
 
-        Renderer renderer = new Renderer(
-                window.getWidth(),
-                window.getHeight()
+    FramebufferPresenter presenter = new FramebufferPresenter(
+            window.getWidth(),
+            window.getHeight()
+    );
+
+    while (!window.shouldClose()) {
+        renderer.render();
+
+        presenter.present(
+                renderer.getPixels()
         );
 
-        FramebufferPresenter presenter = new FramebufferPresenter(
-                window.getWidth(),
-                window.getHeight()
-        );
-
-        while (!window.shouldClose()) {
-            renderer.render();
-
-            presenter.present(
-                    renderer.getPixels()
-            );
-
-            window.update();
-        }
-
-        // exited render loop; clean up and terminate
-        presenter.cleanup();
-        window.cleanup();
+        window.update();
     }
+
+    // exited render loop; clean up and terminate
+    presenter.cleanup();
+    window.cleanup();
 }
